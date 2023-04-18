faa1bd46b819f66f351b93f6c7e668b500d89ebe
Estefanía <estefaniaramirezmnt@gmail.com>
2023-04-18 09:20:48 +0200

    Added filtering array kata

---
+++ Array iteration/filteringNamesOfFriends.js
@@ -0,0 +1,19 @@
+// Make a list of your friends whose names have exatly 4 letters.
+
+function friend (friends) {
+    let listOfFriends = [];
+    for (let person of friends) {
+        if (person.length == 4) {
+            listOfFriends.push(person)
+        }
+    }
+    return listOfFriends
+}
+
+
+function friend2(friends){
+    return friends.filter(n => n.length === 4)
+}
+
+
+console.log(friend(["Love", "Your", "Face", "1"]))
