add612482fb429d06f002d820a5560f1c108f2ba
Estefanía <estefaniaramirezmnt@gmail.com>
2023-11-23 10:22:20 +0100

    Test more completed

--- Easy/Boolean/bonusTime.js
+++ Easy/Boolean/bonusTime.js
@@ -1,22 +1,28 @@
 /*
-Build a function that takes in two arguments (salary, bonus). 
-Salary will be an integer, and bonus a boolean.
+Build a function that takes in two arguments (salary, hasBonus). 
+Salary will be an integer, and hasBonus a boolean.
 
-If bonus is true, the salary should be multiplied by 10. 
-If bonus is false, return only his stated salary.
+If hasBonus is true, the salary should be multiplied by 10. 
+If hasBonus is false, return only his stated salary.
 Examples: 
 1000, true = €10000
 200, false = €200
 */
 
-const bonusTime = (salary, bonus) => bonus===true ? `€${salary * 10}` : `€${salary}`;
+// const BonusTime = (salary, hasBonus) => hasBonus===true ? `€${salary * 10}` : `€${salary}`;
 
-function bonusTime2(salary, bonus) {
-    if (bonus === true) {
-        return salary * 10;
+function BonusTime(salary, hasBonus) {
+    if (typeof(salary) !== "number" || typeof(hasBonus) !== "boolean" ) {
+        return 'Parameters must be a number and a boolean';
+    }
+    if (salary < 0) {
+        return 'Salary must be a positive number';
+    }
+    if (hasBonus === true) {
+        return `€${salary  * 10}`;
     } else {
-        return salary;
+        return `€${salary}`;
     }
 }
 
-module.exports = bonusTime;
+module.exports = BonusTime;
--- Easy/Boolean/bonusTime.test.js
+++ Easy/Boolean/bonusTime.test.js
@@ -5,3 +5,15 @@ test('if true, return the value plus 10, if false only the value', () => {
     expect(bonusTime(1000, true)).toBe('€10000');
     expect(bonusTime(200, false)).toBe('€200');
 })
+
+test('if salary is not a number, return a message', () => {
+    expect(bonusTime('1000', true)).toBe('Parameters must be a number and a boolean');
+})
+
+test('if bonus is not a boolean, return a message', () => {
+    expect(bonusTime(1000, 'true')).toBe('Parameters must be a number and a boolean');
+})
+
+test('if salary is negative, return a message', () => {
+    expect(bonusTime(-1, true)).toBe('Salary must be a positive number');
+})
