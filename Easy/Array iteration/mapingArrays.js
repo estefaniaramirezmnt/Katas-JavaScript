79af06655b529f79e4f3db469ecd1569592512e6
Estefanía <estefaniaramirezmnt@gmail.com>
2024-11-19 14:00:33 +0100

    Changed names for more descriptive ones

--- Easy/Array iteration/mapingArrays.js
+++ Easy/Array iteration/mapingArrays.js
@@ -1,26 +1,19 @@
 // Given an array of integers, return a new array with each value doubled.
 
-function maps(x) {
-    const newArray = x.map(n => n*2);
+function mapingAnArray(array) {
+    const newArray = array.map(number => number *2);
     return newArray;
 }
 
-function maps2(x) {
-    var newArr = [];
-    x.map(function(num) {
-        newArr.push(num*2);
-    })
-    return newArr;
+
+function mapingArray2(array) {
+    return array.map(number => number * 2);
 }
 
-function maps3(x) {
-    return x.map(n => n * 2);
-}
-
-function maps4(x) { // most efficient
-    let arr = [];
-    for (var i = 0; i < x.length; i++) {
-        arr.push(x[i] * 2);
+function mapingArray3(array) { // most efficient
+    let newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        newArray.push(array[i] * 2);
     }
     return arr;
 }
